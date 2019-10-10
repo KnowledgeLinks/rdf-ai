f177ac63febc4483d407e267e53d6c0d60c38f55
Jeremy Nelson <jermnelson@gmail.com>
2019-10-10 09:33:52 -0600

    started creating data frames and updated notebook

--- data_loader/rdf_sample.py
+++ data_loader/rdf_sample.py
@@ -2,7 +2,28 @@ ___author__ = "Jeremy Nelson"
 
 import datetime
 import random
+import rdflib
 import click
+
+import pandas as pd
+
+def rdf_series(graph, subject):
+    data = [str(subject)]
+    index = ['subject']
+    for p,o in graph.predicate_objects(subject=subject):
+        # Predicate is added to index
+        index.append(str(p))
+        # Object is store in data
+        data.append(str(o))
+    return pd.Series(data, index)
+
+def load_graph(graph):
+    data_series = []
+    # Loads triples into subject data series
+    for subject in set(graph.subjects()):
+        data_series.append(rdf_series(graph, subject))
+    # For now returns a list, should create data frames for each rdf:type
+    return data_series
 
 @click.command()
 def interactive():
